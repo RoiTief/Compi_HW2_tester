ea386cde578ce5eea2addf646a052cd70a81b7d4
RoiTief <roitief@post.bgu.ac.il>
2022-12-14 23:46:18 +0200

    init

---
+++ tp_tests.ml
@@ -0,0 +1,21 @@
+(* expects tag_parser to complete successfully
+ * 'test' - original s-expression
+ * 'expected_result' - output of (Tag_Parser.sprint_expr () (Tag_Parser.tag_parser (Reader.nt_sexpr test 0))) if tagged successfully
+ * *)
+type tp_success_test = {test: string; expected_result: string};;
+
+(* expects tag_parser to raise exception during parsing
+ * 'test' - original s-expression
+ * *)
+type tp_failure_test = {test: string};;
+
+let tp_success_tests = [
+  {test = "(letrec () (+ 1 2) (- 2 1))"; expected_result = "(let () (begin (+ 1 2) (- 2 1)))"};
+  {test = "(and)" ; expected_result = "#t"}
+];;
+
+let tp_failure_tests = [
+  {test = "(let (and 5) and)"};
+  {test = "(define and 5)"}
+];;
+
